fe3fb962d6366330aa611cad373151455004d3e7
Beau Ingram <12903225@student.uts.edu.au>
2019-09-20 13:46:44 +1000

    indexes and templates setup

--- DataSystems_Project3/Project3App2/views.py
+++ DataSystems_Project3/Project3App2/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to the internet")
+    now = datetime.now()
+
+    return render(
+        request,
+        "Project3App2/index.html",
+        {
+            'title': "The internet",
+            'message': "The internethmmmm",
+            'content': " is: " + now.strftime("%A, %d, %B, %Y at %X")     
+        }
+        )
