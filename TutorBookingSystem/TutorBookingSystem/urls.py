fea372d51a128e74254dbaf99b81b2d4409e4e6f
Beau Ingram <12903225@student.uts.edu.au>
2019-09-23 02:16:47 +1000

    comment for old to new urls method comparison

--- TutorBookingSystem/TutorBookingSystem/urls.py
+++ TutorBookingSystem/TutorBookingSystem/urls.py
@@ -28,3 +28,39 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
 ]
+
+"""
+OLDER METHOD:
+
+from datetime import datetime
+from django.conf.urls import url
+import django.contrib.auth.views
+
+import app.forms
+import app.views
+
+urlpatterns = [
+    url(r'^$', app.views.home, name='home'),
+    url(r'^contact$', app.views.contact, name='contact'),
+    url(r'^about$', app.views.about, name='about'),
+    url(r'^login/$',
+        django.contrib.auth.views.login,
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': app.forms.BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title': 'Log in',
+                'year': datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        django.contrib.auth.views.logout,
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+]
+
+"""
