c49eb61a5ef74af99f77fdbd541a3c883371cd0b
Beau Ingram <12903225@student.uts.edu.au>
2019-10-04 14:26:20 +1000

    db migrations

--- DjangoPolls/app/urls.py
+++ DjangoPolls/app/urls.py
@@ -7,19 +7,27 @@ from app.models import Poll
 import app.views
 
 urlpatterns = [
+    # Home page routing
     path('',
         app.views.PollListView.as_view(
             queryset=Poll.objects.order_by('-pub_date')[:5],
             context_object_name='latest_poll_list',
             template_name='app/index.html',),
         name='home'),
+    # Routing for a poll page, which use URLs in the form <poll_id>/,
+    # where the id number is captured as a group named "pk".
     path('<int:pk>/',
         app.views.PollDetailView.as_view(
             template_name='app/details.html'),
         name='detail'),
+    # Routing for <poll_id>/results pages, again using a capture group
+    # named pk.
     path('<int:pk>/results/',
         app.views.PollResultsView.as_view(
             template_name='app/results.html'),
         name='results'),
+    # Routing for <poll_id>/vote pages, with the capture group named
+    # poll_id this time, which becomes an argument passed to the view.
     path('<int:poll_id>/vote/', app.views.vote, name='vote'),
+    path('seed', app.views.seed, name='seed'),
 ]
